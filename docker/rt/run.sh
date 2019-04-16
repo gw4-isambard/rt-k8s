0e32fe94078a6a8032f342bea06d37d3b8a02a22
James Price <j.price@bristol.ac.uk>
2019-04-16 19:42:30 +0100

    Add fetchmail configuration

--- docker/rt/run.sh
+++ docker/rt/run.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
 envsubst '$DATABASE_PASSWORD' </opt/rt4/etc/RT_SiteConfig.pm.in >/opt/rt4/etc/RT_SiteConfig.pm
+envsubst '$O365_USER $O365_PASS' </etc/fetchmailrc.in >/etc/fetchmailrc
 
+# Launch fetchmail daemon
+touch /var/log/fetchmail.log
+chmod og-rwx /etc/fetchmailrc
+fetchmail -f /etc/fetchmailrc
+
+# Lauch webserver
 /usr/sbin/httpd -DFOREGROUND
