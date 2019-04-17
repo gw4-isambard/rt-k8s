171ec4b948cfd5d105fc090bb63ffb0efc2b2964
James Price <j.price@bristol.ac.uk>
2019-04-17 12:50:56 +0100

    Launch fetchmail in the background

--- docker/rt/run.sh
+++ docker/rt/run.sh
@@ -6,7 +6,7 @@ envsubst '$O365_USER $O365_PASS $SERVICE_ADDRESS' </etc/fetchmailrc.in >/etc/fet
 # Launch fetchmail daemon
 touch /var/log/fetchmail.log
 chmod og-rwx /etc/fetchmailrc
-fetchmail -f /etc/fetchmailrc
+fetchmail -f /etc/fetchmailrc &
 
 # Lauch webserver
 /usr/sbin/httpd -DFOREGROUND
